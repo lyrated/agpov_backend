3e602ae6332cfe34496fd0090dd0a9ea76fcde97
Ly Nguyen <mly.nguyen@yahoo.de>
2021-08-18 23:24:37 +0200

    separating development from production

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
 
 app.use('/', require('./routes/index'));
 app.use('/api', require('./routes/timeChart'));
