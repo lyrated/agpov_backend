a022327545a13f25583642bbc1f3ebc1e6f37bd9
Ly Nguyen <mly.nguyen@yahoo.de>
2021-08-19 21:26:16 +0200

    fixed routing

--- app.js
+++ app.js
@@ -30,23 +30,28 @@ app.use('/api', require('./routes/timeChart'));
 app.use('/api', require('./routes/genresDepartments'));
 app.use('/api', require('./routes/budgetRevenue'));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+if (process.env.NODE_ENV === 'development') {
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
 
 if (process.env.NODE_ENV === 'production') {
+  app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/build/index.html'));
+  });
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/build/index.html'));
   });
